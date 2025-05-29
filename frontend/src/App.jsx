eba0696a16f7a1f263edb15500bade8450f5edfc
Prakash7989 <prakashbhukya224@gmail.com>
2025-05-29 11:01:19 +0530

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+const App = () => {
+  return (
+    <div className='text-center'>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+      <p>Feel free to modify the code and see the changes!</p>
+      <p>Happy coding!</p>
+    </div>
+  )
+}
+
+export default App
